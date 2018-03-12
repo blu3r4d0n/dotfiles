37523b0de93b59d84a3993d6e995607359c5b1d4
Philip Nelson <philipnelson99@icloud.com>
2018-03-12 01:20:28 -0400

    lazy commit

---
+++ polybar/inbox-imap-python.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import imaplib
+
+obj = imaplib.IMAP4_SSL('imap.gmail.com', 993)
+obj.login(***REMOVED***, 'Pnelson4096')
+obj.select()
+
+print(len(obj.search(None, 'unseen')[1][0].split()))
+
