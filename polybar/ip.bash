2945092653e782da2964b266eb154ce49d32250a
Philip Nelson <philipnelson99@icloud.com>
2018-03-12 01:20:28 -0400

    lazy commit

--- polybar/ip.bash
+++ polybar/ip.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
-ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+#ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+ip=$(ifconfig wlp3s0 | awk 'FNR==2' | awk '{print $2}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 echo $ip
